c2767b200818535215225d57268eed2a7af62a5d
Mohammedhaq <mohammedzainhaq@gmail.com>
2025-06-03 19:45:06 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -2,12 +2,36 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 console.log(process.env.MONGO_URI);
+
+app.post("/api/products", (req, res) => {
+    const product = req.body;
+
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "All fields are required" });
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        newProduct.save();
+        res.status(201).json({ success: true, message: "Product created successfully", product: newProduct });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
+
+
 
 app.listen(5000, () => {
     connectDB();
